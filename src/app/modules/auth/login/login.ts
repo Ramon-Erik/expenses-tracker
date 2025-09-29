927f4744aedbbe53dd63aef582c4f104b4856be4
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-09-28 22:20:09 -0300

    feat: logo

--- src/app/modules/auth/login/login.ts
+++ src/app/modules/auth/login/login.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Logo } from "../../../shared/components/logo/logo";
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [Logo],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
