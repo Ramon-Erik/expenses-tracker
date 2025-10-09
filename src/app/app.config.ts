ab83e68b0f363e9a8b7ce66c6436cb1df3600e3d
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-10-09 20:24:09 -0300

    style: format document and removed unused props

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -10,6 +14,20 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "expenses-track-auth", appId: "1:309455920121:web:52d1dfab85e4a038253b28", storageBucket: "expenses-track-auth.firebasestorage.app", apiKey: "AIzaSyC4_0EJSx2LJ6k5fRwSZO7m87XyC07sDMo", authDomain: "expenses-track-auth.firebaseapp.com", messagingSenderId: "309455920121", projectNumber: "309455920121", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
-  ]
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'expenses-track-auth',
+        appId: '1:309455920121:web:52d1dfab85e4a038253b28',
+        storageBucket: 'expenses-track-auth.firebasestorage.app',
+        apiKey: 'AIzaSyC4_0EJSx2LJ6k5fRwSZO7m87XyC07sDMo',
+        authDomain: 'expenses-track-auth.firebaseapp.com',
+        messagingSenderId: '309455920121',
+        // projectNumber: '309455920121',
+        // version: '2',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
