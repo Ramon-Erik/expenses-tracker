3c2bf9c866e107141bad5aa7c77abc45b2ce75d2
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-10-09 20:02:39 -0300

    core: downloaded sucessfully firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "expenses-track-auth", appId: "1:309455920121:web:52d1dfab85e4a038253b28", storageBucket: "expenses-track-auth.firebasestorage.app", apiKey: "AIzaSyC4_0EJSx2LJ6k5fRwSZO7m87XyC07sDMo", authDomain: "expenses-track-auth.firebaseapp.com", messagingSenderId: "309455920121", projectNumber: "309455920121", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
   ]
 };
