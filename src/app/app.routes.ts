15b8be59a5850a5196eff3a49a79d15e0923330e
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-10-10 09:41:28 -0300

    feat: routes for home children and login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,13 @@ export const routes: Routes = [
   {
     path: 'login',
     loadComponent: () => import('./modules/auth/login/login').then(comp => comp.Login)
-  }
+  },
+  {
+    path: '',
+    loadChildren: () => import('./modules/home/home.routes').then(m => m.routes)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./modules/home/home.routes').then(m => m.routes)
+  },
 ];
