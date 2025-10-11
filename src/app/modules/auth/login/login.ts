c4c69d8ead29c0fe8d917b13e1e7b5b233ad60db
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-10-11 19:58:20 -0300

    feat: added login function

--- src/app/modules/auth/login/login.ts
+++ src/app/modules/auth/login/login.ts
@@ -2,23 +2,47 @@ import { Component, inject, Input } from '@angular/core';
 import { Logo } from "../../../shared/components/logo/logo";
 import { InputDefault } from '../../../shared/components/inputs/input-default/input-default';
 import { FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth/auth.service';
+import { take, tap } from 'rxjs';
+import { Router } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [Logo, InputDefault, FormsModule, ReactiveFormsModule],
+  imports: [Logo, InputDefault, FormsModule, ReactiveFormsModule, AsyncPipe],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
   #fb = inject(FormBuilder)
+  #authService = inject(AuthService)
+  #router = inject(Router)
+
+  public authError  = this.#authService.getErrorAuth()
+  public loading = false;
 
   public loginForm = this.#fb.group({
-    email: [null, [Validators.required, Validators.email]],
-    password: [null, [Validators.required, Validators.minLength(8)]]
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]]
   })
 
-  public log() {
-    console.log(this.loginForm);
-    
+  public login() {
+    this.loading = true;
+    if (this.loginForm.valid) {
+      const email = this.loginForm.controls['email'].value!
+      const password = this.loginForm.controls['password'].value!
+      this.#authService.login(email, password)
+        .pipe(
+          take(1)
+        )
+        .subscribe({
+          next: () => {
+            this.#router.navigate(['/home'])
+          },
+          complete: () => {
+            this.loading = false;
+          }
+        })
+    }
   }
 }
