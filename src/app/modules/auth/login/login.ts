85a64cd8bd79942205b4a606d5e063985ca79655
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-10-11 15:32:02 -0300

    feat: reusable and reactive inputs forms

--- src/app/modules/auth/login/login.ts
+++ src/app/modules/auth/login/login.ts
@@ -1,13 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Logo } from "../../../shared/components/logo/logo";
 import { InputDefault } from '../../../shared/components/inputs/input-default/input-default';
+import { FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [Logo, InputDefault],
+  imports: [Logo, InputDefault, FormsModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
+  #fb = inject(FormBuilder)
 
+  public loginForm = this.#fb.group({
+    email: [null, [Validators.required, Validators.email]],
+    password: [null, [Validators.required, Validators.minLength(8)]]
+  })
+
+  public log() {
+    console.log(this.loginForm);
+    
+  }
 }
--- src/app/shared/components/inputs/input-default/input-default.ts
+++ src/app/shared/components/inputs/input-default/input-default.ts
@@ -1,15 +1,43 @@
-import { Component, input } from '@angular/core';
+import { Component, forwardRef, inject, input } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, NgControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input-default',
-  imports: [],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => InputDefault),
+    multi: true
+  }],
+  imports: [FormsModule],
   templateUrl: './input-default.html',
   styleUrl: './input-default.scss'
 })
-export class InputDefault {
+export class InputDefault implements ControlValueAccessor {
   public id = input.required<string>();
   public type = input.required<string>();
   public label = input.required<string>();
   public isRequired = input(false);
   public placeholder = input('');
+
+  public inputValue = '';
+
+  protected onToutched? : () => {}
+  protected onChange? : (value: string) => {}
+  protected isDisabled = false
+  
+  writeValue(obj: string): void {
+    this.inputValue = obj
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onToutched = fn
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled
+  }
+
 }
