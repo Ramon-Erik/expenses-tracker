b34505baea44f1b2e45443b381051e4264af5793
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-10-22 13:44:52 -0300

    ferat: simple home template

--- src/app/modules/home/home/home.ts
+++ src/app/modules/home/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { InputDefault } from "../../../shared/components/inputs/input-default/input-default";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [InputDefault],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
