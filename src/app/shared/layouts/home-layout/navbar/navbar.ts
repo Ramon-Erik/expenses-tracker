d4c6164924fb2c16c06b08aec1d7b365d231efef
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-10-23 11:13:16 -0300

    feat: created component menu and styles

--- src/app/shared/layouts/home-layout/navbar/navbar.ts
+++ src/app/shared/layouts/home-layout/navbar/navbar.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Logo } from "../../../components/logo/logo";
 import { Profile } from "./components/profile/profile";
+import { Menu } from "./components/menu/menu";
 
 @Component({
   selector: 'app-navbar',
-  imports: [Logo, Profile],
+  imports: [Logo, Profile, Menu],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
