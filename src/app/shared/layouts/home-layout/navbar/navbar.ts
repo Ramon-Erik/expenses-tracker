cf220b0a3b5f779e0a62bbe25b65ce728b776606
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-10-23 11:05:15 -0300

    feat: component profile with styles

--- src/app/shared/layouts/home-layout/navbar/navbar.ts
+++ src/app/shared/layouts/home-layout/navbar/navbar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Logo } from "../../../components/logo/logo";
+import { Profile } from "./components/profile/profile";
 
 @Component({
   selector: 'app-navbar',
-  imports: [Logo],
+  imports: [Logo, Profile],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
