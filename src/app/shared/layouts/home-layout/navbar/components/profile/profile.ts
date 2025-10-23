243169a57b89c93b78862e813b75caff4d4d31e0
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-10-23 12:07:08 -0300

    feat: profile options and logout function

--- src/app/shared/layouts/home-layout/navbar/components/profile/profile.ts
+++ src/app/shared/layouts/home-layout/navbar/components/profile/profile.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../../../../core/services/auth/auth.service';
+import { catchError, of, take } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrl: './profile.scss'
 })
 export class Profile {
-
+  #authService = inject(AuthService)
+  #router = inject(Router)
+  public openProfileOptions() {}
+  public logout() {
+    this.#authService.logout().pipe(
+      take(1),
+      catchError(error => {
+        console.error('Erro ao fazer logout:', error);
+        return of(null)
+      })
+    ).subscribe(logout => 
+      this.#router.navigate(['/login'])
+    )
+  }
 }
