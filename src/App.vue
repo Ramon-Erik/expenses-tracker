6b150a37c3dbba7a314e79b27d3449f850086433
Ramon Silva <ramonerik29a@gmail,com>
2026-04-16 13:52:13 -0300

    feat: moving balance to wallet view instead of app view

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <ExHeader />
     <div class="container">
       <ExMenu />
-      <ExBalance :total="0" />
       <router-view />
     </div>
   </div>
@@ -14,32 +13,6 @@
 import ExHeader from './components/ExHeader.vue'
 import ExFooter from './components/ExFooter.vue'
 import ExMenu from './components/ExMenu.vue'
-import ExBalance from './components/ExBalance.vue'
-/*
-import type ITransaction from './interfaces/ITransaction.interface'
-
-import { computed, ref } from 'vue'
-
-const LOCAL_KEY = 'exp-tracker'
-
-const getLocalList = () => {
-  const list = localStorage.getItem(LOCAL_KEY) ? JSON.parse(localStorage.getItem(LOCAL_KEY)!) : []
-
-  return list as ITransaction[]
-} */
-
-// const transactionsList = ref<ITransaction[]>(getLocalList())
-
-/* const onNewTransaction = (newTransaction: ITransaction) => {
-  updateLocalList(() => {
-    transactionsList.value.push(newTransaction)
-  })
-} */
-
-/* const total = computed(() => {
-  const reduceFn = (acc: number, tr: ITransaction) => acc + tr.amount
-  return transactionsList.value.reduce(reduceFn, 0)
-}) */
 </script>
 
 <style scoped></style>
