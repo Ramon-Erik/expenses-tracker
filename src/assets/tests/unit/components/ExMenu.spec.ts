e4cc6e7ec5db5868769b35a451d372aca228f5f1
Ramon Silva <ramonerik29a@gmail,com>
2026-04-22 14:47:08 -0300

    feat: tests for menu component

--- src/assets/tests/unit/components/ExMenu.spec.ts
+++ src/assets/tests/unit/components/ExMenu.spec.ts
@@ -1,25 +1,41 @@
 import ExMenu from '@/components/ExMenu.vue'
 import { mount } from '@vue/test-utils'
+import { createRouter, createWebHistory, type Router } from 'vue-router'
 
 describe('Menu', () => {
-  it('Should render a unordenated list', async () => {
-    const mockRoute = {
-      path: '/',
-    }
-    const mockRouter = {
-      push: jest.fn(),
-    }
+  let router: Router
+  beforeEach(() => {
+    router = createRouter({
+      history: createWebHistory(),
+      routes: [
+        { path: '/', component: { template: '<div>Home</div>' } },
+        { path: '/simulate', component: { template: '<div>Carrinho</div>' } },
+      ],
+    })
+  })
 
+  it('Should render a unordenated list with two items', async () => {
     const wrapper = mount(ExMenu, {
       global: {
-        mocks: {
-          $route: mockRoute,
-          $router: mockRouter,
-        },
+        plugins: [router],
       },
     })
-
-  
     expect(wrapper.html()).toContain('ul')
+    expect(wrapper.findAll('li').length).toBe(2)
+  })
+
+  it('Should set the clicked link as active', async () => {
+    router.push('/simulate')
+    await router.isReady()
+
+    const wrapper = mount(ExMenu, {
+      global: { plugins: [router] },
+    })
+
+    await wrapper.vm.$nextTick()
+
+    const activetedLink = wrapper.find('a[href="/simulate"]')
+
+    expect(activetedLink.classes()).toContain('active')
   })
 })
