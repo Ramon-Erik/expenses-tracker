bd7eceaf2b72fdfae48726fd8d3ba7fb1b29b956
Ramon Silva <ramonerik29a@gmail,com>
2026-04-28 17:37:20 -0300

    feat: expecting four elements in simulate view

--- tests/unit/views/simulate/SimulateCartView.spec.ts
+++ tests/unit/views/simulate/SimulateCartView.spec.ts
@@ -4,13 +4,17 @@ import { shallowMount } from "@vue/test-utils";
 describe("SimulateCartView", () => {
   const wrapper = shallowMount(SimulateCartView);
 
+  it("should render four component", () => {
+    expect(wrapper.findAll(".container > *").length).toBe(4);
+  });
+
   it("should render Amount component", () => {
     expect(wrapper.findComponent({ name: "ExAmount" }).exists()).toBe(true);
   });
 
   it("should render ProductsList component", () => {
     expect(wrapper.findComponent({ name: "ExProductsList" }).exists()).toBe(
-      true
+      true,
     );
   });
 
