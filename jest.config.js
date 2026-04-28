9a245bdb80baf7d66719e4317402cb435da183b6
Ramon Silva <ramonerik29a@gmail,com>
2026-04-28 15:07:09 -0300

    chore: coverage on untested files

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+
+  collectCoverage: true,
+
+  collectCoverageFrom: [
+    "src/**/*.{ts,vue}",
+    "!src/main.ts",
+    "!src/router/index.ts",
+    "!src/types-fix.d.ts",
+    "!src/shims-vue.d.ts",
+    "!node_modules/**",
+    "!src/**/index.ts",
+  ],
+
+  coverageReporters: ["text", "lcov", "clover"],
 };
