85d9e98ead6caeade627f7e9da8deea743479692
Ramon Silva <ramonerik29a@gmail,com>
2026-04-30 14:59:20 -0300

    feat: testing balance component

---
+++ tests/unit/views/wallet/components/ExBalance.spec.ts
@@ -0,0 +1,55 @@
+import { useTransactionsStore } from "@/stores/TransactionsStore";
+import { currencyFormat } from "@/utils/currency";
+import ExBalance from "@/views/wallet/components/ExBalance.vue";
+import { createTestingPinia } from "@pinia/testing";
+import { mount, VueWrapper } from "@vue/test-utils";
+
+describe("ExBalance", () => {
+  const mountComponent = (configs = {}) => {
+    return mount(ExBalance, {
+      global: {
+        plugins: [
+          createTestingPinia({
+            stubActions: false,
+          }),
+        ],
+      },
+      ...configs,
+    });
+  };
+
+  it("should have a paragraph with total amount", async () => {
+    const wrapper: VueWrapper<InstanceType<typeof ExBalance>> =
+      mountComponent();
+    const store = useTransactionsStore();
+
+    beforeEach(() => {
+      createTestingPinia();
+    });
+
+    const expenses = [
+      {
+        id: 1,
+        description: "string",
+        amount: 100,
+        isIncome: true,
+      },
+      {
+        id: 2,
+        description: "mock",
+        amount: 10,
+        isIncome: false,
+      },
+    ];
+    const totalMoney = expenses.reduce(
+      (acc, nextTransaction) => acc + nextTransaction.amount,
+      0
+    );
+
+    expenses.some((ex) => store.addTransaction(ex));
+
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.find("p#balance")).toContain(currencyFormat(totalMoney));
+  });
+});
