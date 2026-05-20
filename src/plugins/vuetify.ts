50bd0e4dcba0aff493ba296b2e1f83652a757f5b
Ramon Silva <ramonerik29a@gmail,com>
2026-05-20 14:53:26 -0300

    feat: theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -6,9 +6,25 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import { VDateInput } from "vuetify/labs/VDateInput";
 
+const darkTheme = {
+  dark: true,
+  colors: {
+    background: "#0D1117",
+    surface: "#161B22",
+
+    primary: "#238636",
+  },
+};
+
 export default createVuetify({
   components: {
     VDateInput,
+  },
+  theme: {
+    defaultTheme: "darkTheme",
+    themes: {
+      darkTheme,
+    },
   },
 });
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
