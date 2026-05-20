81804d3d93aa172149b470ba90cebbe78e1e7b68
Ramon Silva <ramonerik29a@gmail,com>
2026-05-20 18:28:32 -0300

    feat: enhance new transaction form with validation and improved structure

--- src/views/wallet/new-transaction/ExNewTransaction.vue
+++ src/views/wallet/new-transaction/ExNewTransaction.vue
@@ -4,137 +4,158 @@
       <h2>Nova movimentação</h2>
     </v-card-title>
     <v-card-text>
-      <v-text-field
-        v-model="form.description"
-        label="Descrição"
-        variant="outlined"
-        placeholder="Ex: Salário, Transporte"
-        persistent-placeholder
-        clearable
-      ></v-text-field>
-      <v-expand-transition mode="out-in">
-        <div v-show="showForm" class="transaction-info pt-3">
-          <v-row>
-            <v-col cols="6">
-              <v-number-input
-                v-model="form.amount"
-                variant="outlined"
-                label="Valor total (R$)"
-                placeholder="0.00"
-                control-variant="hidden"
-                persistent-placeholder
-                hide-details
-              ></v-number-input>
-            </v-col>
-            <v-col cols="6">
-              <p>Tipo</p>
-              <v-switch
-                v-model="form.isIncoming"
-                label="Entrada"
-                color="green"
-                hide-details
-              ></v-switch>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12" sm="6">
-              <v-date-input
-                v-model="form.inicialDate"
-                hide-details
-                prepend-icon=""
-                prepend-inner-icon="$calendar"
-                variant="outlined"
-                label="Data de início"
-              ></v-date-input>
-            </v-col>
-            <v-col cols="12" sm="6">
-              <v-date-input
-                v-model="form.finalDate"
-                hide-details
-                prepend-icon=""
-                prepend-inner-icon="$calendar"
-                variant="outlined"
-                label="Data de fim"
-              ></v-date-input>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12" sm="6">
-              <v-select
-                v-model="form.category"
-                hide-details
-                variant="outlined"
-                label="Categoria"
-                :items="[
-                  'California',
-                  'Colorado',
-                  'Florida',
-                  'Georgia',
-                  'Texas',
-                  'Wyoming',
-                ]"
-              ></v-select>
-            </v-col>
-            <v-col cols="12" sm="6">
-              <v-select
-                v-model="form.paymentMethod"
-                hide-details
-                variant="outlined"
-                label="Método de pagamento"
-                :items="[
-                  'California',
-                  'Colorado',
-                  'Florida',
-                  'Georgia',
-                  'Texas',
-                  'Wyoming',
-                ]"
-              ></v-select>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12">
-              <p>Tags</p>
-              <v-chip-group
-                v-model="form.tags"
-                selected-class="text-primary"
-                multiple
-                column
-              >
-                <v-chip
-                  v-for="tag in tags"
-                  :key="tag.value"
-                  class="text-capitalize"
-                  :value="tag.value"
-                  :text="tag.text"
-                  filter
-                ></v-chip>
-                <v-btn
-                  class="ma-auto ml-0 text-capitalize"
-                  density="comfortable"
-                  rounded="xl"
-                  prepend-icon="mdi-plus"
-                  >Nova Tag</v-btn
+      <v-form
+        v-model="form.valid"
+        :ref="(el: VFormRef) => form.ref = el"
+        @submit.prevent="handleSubmit"
+      >
+        <v-text-field
+          v-model="form.values.description"
+          label="Descrição"
+          variant="outlined"
+          placeholder="Ex: Salário, Transporte"
+          :rules="[rules.required, rules.minLength(3), rules.maxLength(30)]"
+          persistent-placeholder
+          clearable
+        ></v-text-field>
+        <v-expand-transition mode="out-in">
+          <div v-show="showForm" class="transaction-info pt-3">
+            <v-row>
+              <v-col cols="6">
+                <v-number-input
+                  v-model="form.values.amount"
+                  variant="outlined"
+                  label="Valor total (R$)"
+                  placeholder="0.00"
+                  control-variant="hidden"
+                  :rules="[rules.required, rules.minValue(0)]"
+                  persistent-placeholder
+                ></v-number-input>
+              </v-col>
+              <v-col cols="6">
+                <p>Tipo</p>
+                <v-switch
+                  v-model="form.values.isIncoming"
+                  label="Entrada"
+                  color="green"
+                ></v-switch>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12" sm="6">
+                <v-date-input
+                  v-model="form.values.inicialDate"
+                  prepend-icon=""
+                  prepend-inner-icon="$calendar"
+                  variant="outlined"
+                  label="Data de início"
+                  :rules="[rules.required]"
+                ></v-date-input>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-date-input
+                  v-model="form.values.finalDate"
+                  prepend-icon=""
+                  prepend-inner-icon="$calendar"
+                  variant="outlined"
+                  label="Data final"
+                  :rules="[rules.finalDate]"
+                ></v-date-input>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12" sm="6">
+                <v-select
+                  v-model="form.values.category"
+                  variant="outlined"
+                  label="Categoria"
+                  :items="[
+                    'California',
+                    'Colorado',
+                    'Florida',
+                    'Georgia',
+                    'Texas',
+                    'Wyoming',
+                  ]"
+                  :rules="[rules.required]"
+                ></v-select>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-select
+                  v-model="form.values.paymentMethod"
+                  variant="outlined"
+                  label="Método de pagamento"
+                  :items="[
+                    'California',
+                    'Colorado',
+                    'Florida',
+                    'Georgia',
+                    'Texas',
+                    'Wyoming',
+                  ]"
+                  :rules="[rules.required]"
+                ></v-select>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <p>Tags (escolha pelo menos uma)</p>
+                <v-chip-group
+                  v-model="form.values.tags"
+                  selected-class="text-primary"
+                  multiple
+                  column
+                  mandatory
                 >
-              </v-chip-group>
-            </v-col>
-          </v-row>
-          <v-row class="justify-space-between">
-            <v-col>
-              <v-btn @click="clearForm">Limpar</v-btn>
-            </v-col>
-            <v-col cols="auto">
-              <v-btn prepend-icon="mdi-plus" color="green">Adicionar</v-btn>
-            </v-col>
-          </v-row>
-        </div>
-      </v-expand-transition>
+                  <v-chip
+                    v-for="tag in tags"
+                    :key="tag.value"
+                    class="text-capitalize"
+                    :value="tag.value"
+                    :text="tag.text"
+                    filter
+                  ></v-chip>
+                  <v-btn
+                    class="ma-auto ml-0 text-capitalize"
+                    density="comfortable"
+                    rounded="xl"
+                    prepend-icon="mdi-plus"
+                    >Nova Tag</v-btn
+                  >
+                </v-chip-group>
+              </v-col>
+            </v-row>
+            <v-row class="justify-space-between">
+              <v-col>
+                <v-btn @click="clearForm">
+                  Limpar
+                  <v-tooltip activator="parent" location="end">
+                    Esconde esse formulário
+                  </v-tooltip>
+                </v-btn>
+              </v-col>
+              <v-col cols="auto">
+                <v-btn
+                  type="submit"
+                  :disabled="!form.valid"
+                  prepend-icon="mdi-plus"
+                  color="green"
+                  >Adicionar</v-btn
+                >
+              </v-col>
+            </v-row>
+          </div>
+        </v-expand-transition>
+      </v-form>
     </v-card-text>
   </v-card>
 </template>
 
 <script setup lang="ts">
 import { ref, watch } from "vue";
+import type { VForm } from "vuetify/components";
+
+type VFormRef = InstanceType<typeof VForm>;
 
 const tags = ref([
   { text: "estágio - ida", value: 0 },
@@ -146,27 +167,61 @@ const formDefault = {
   description: "",
   amount: null,
   isIncoming: false,
-  inicialDate: "04/28/2026",
+  inicialDate: new Date(),
   finalDate: null,
   category: null,
   paymentMethod: null,
   tags: [],
 };
 
-const form = ref({ ...formDefault });
+const form = ref({
+  values: { ...formDefault },
+  valid: null as boolean | null,
+  ref: null as VFormRef | null,
+});
 const showForm = ref(false);
 
+const rules = {
+  required: (v: unknown) => !!v || "Este campo é obrigatório",
+  minValue: (min: number) => {
+    return (v: number) => v > min || `Valores a partir de ${min}`;
+  },
+  minLength: (min: number) => {
+    return (v: string) => v.length >= min || `Mínimo de ${min} caracteres`;
+  },
+  maxLength: (max: number) => {
+    return (v: string) => v.length <= max || `Máximo de ${max} caracteres`;
+  },
+  finalDate: (v: string) => {
+    if (!v) return true;
+
+    const inicialDate = form.value.values.inicialDate;
+    if (!inicialDate) return "Selecione a data de início antes";
+
+    const start = new Date(inicialDate).getTime();
+    const end = new Date(v).getTime();
+
+    return start < end || "A data final deve ser maior que a inicial";
+  },
+};
+
+const clearForm = () => {
+  if (form.value.ref) form.value.ref.reset();
+
+  form.value.values = { ...formDefault };
+  showForm.value = false;
+};
+
+const handleSubmit = () => {
+  console.log(form.value);
+};
+
 watch(
-  () => form.value.description,
+  () => form.value.values.description,
   (newDescription) => {
     if (newDescription && newDescription.length) {
       showForm.value = true;
     }
   }
 );
-
-const clearForm = () => {
-  form.value = { ...formDefault };
-  showForm.value = false;
-};
 </script>
