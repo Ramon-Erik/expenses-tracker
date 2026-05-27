69d0ff1ae990a0827eabd75e549e7f59abf9874b
Ramon Silva <ramonerik29a@gmail,com>
2026-05-27 16:26:43 -0300

    chore: added gh-pages abd deploy commands

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/expenses-tracker/" : "/",
+
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
