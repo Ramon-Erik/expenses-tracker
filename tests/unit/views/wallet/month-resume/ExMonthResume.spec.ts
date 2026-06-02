e04aba4786a1826d2a31ba0e38fcfcc90e986384
Ramon Silva <ramonerik29a@gmail,com>
2026-06-02 16:24:02 -0300

    feat: refactor ExMonthResume tests to use mountComponentWithVuetify for improved consistency

--- tests/unit/views/wallet/month-resume/ExMonthResume.spec.ts
+++ tests/unit/views/wallet/month-resume/ExMonthResume.spec.ts
@@ -1,31 +1,23 @@
 import ExMonthResume from "@/views/wallet/month-resume/ExMonthResume.vue";
-import { mount } from "@vue/test-utils";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/dist/vuetify";
-import * as directives from "vuetify/dist/vuetify";
-
-globalThis.ResizeObserver = require("resize-observer-polyfill");
-
-const vuetify = createVuetify({ components, directives });
+import { mountComponentWithVuetify } from "../../../../test-utils/mountComponent";
 
 describe("ExMonthResume", () => {
-  const mountComponent = () => {
-    return mount(ExMonthResume, {
-      shallow: true,
-      global: {
-        plugins: [vuetify],
-        components: (components as any).components,
-      },
-    });
-  };
-  it("deveria mostrar o mês e ano atual", () => {
-    const wrapper = mountComponent();
+  const wrapper = mountComponentWithVuetify(ExMonthResume, {
+    global: { stubs: { ExMonthBalance: true } },
+  });
 
+  it("deveria mostrar o mês e ano atual", () => {
     const date = new Date().toLocaleString("pt-BR", {
       month: "long",
       year: "numeric",
     });
 
     expect(wrapper.text()).toContain(date);
+  });
+
+  it("deveria carregar o componente MonthBalance", () => {
+    expect(wrapper.findComponent({ name: "ExMonthBalance" }).exists()).toBe(
+      true
+    );
   });
 });
