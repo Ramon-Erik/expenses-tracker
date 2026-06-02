e6ddd7b11052c061a23d5f5ec8b2cc646d8feb3f
Ramon Silva <ramonerik29a@gmail,com>
2026-06-02 16:39:57 -0300

    feat: refactor WalletView tests to use mountComponentWithVuetify for improved component rendering

--- tests/unit/views/wallet/WalletView.spec.ts
+++ tests/unit/views/wallet/WalletView.spec.ts
@@ -1,11 +1,32 @@
 import WalletView from "@/views/wallet/WalletView.vue";
-import { shallowMount } from "@vue/test-utils";
+import { mountComponentWithVuetify } from "../../../test-utils/mountComponent";
 
 describe("WalletView", () => {
-  it("Should render only trhee components", () => {
-    const wrapper = shallowMount(WalletView, { shallow: true });
-    const components = wrapper.findAll(".container > *");
+  const wrapper = mountComponentWithVuetify(WalletView, {
+    global: {
+      stubs: {
+        ExMonthResume: true,
+        ExNewTransaction: true,
+        ExTransactionsList: true,
+      },
+    },
+  });
 
-    expect(components.length).toBe(2);
+  it("deveria renderizar MonthResume", () => {
+    expect(wrapper.findComponent({ name: "ExMonthResume" }).exists()).toBe(
+      true
+    );
+  });
+
+  it("deveria renderizar NewTransaction", () => {
+    expect(wrapper.findComponent({ name: "ExNewTransaction" }).exists()).toBe(
+      true
+    );
+  });
+
+  it("deveria renderizar TransactionsList", () => {
+    expect(wrapper.findComponent({ name: "ExTransactionsList" }).exists()).toBe(
+      true
+    );
   });
 });
